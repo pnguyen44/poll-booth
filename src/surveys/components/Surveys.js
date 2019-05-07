ff6aee0e8d57ba7fffa863f97561da78d38939fe
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-07 19:51:05 -0400

    Add Surveys component

---
+++ src/surveys/components/Surveys.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Surveys extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+      <p>Surveys...</p>
+      </React.Fragment>
+    )
+  }
+}
+
+
+export default Surveys
