2e58174e1b0cf42e9bcc6299f2e2c496815c973f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-07 19:51:24 -0400

    Add Surveys component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import Header from './header/Header'
 // import { Route, Link, Switch } from 'react-router-dom'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import blue from '@material-ui/core/colors/blue'
+import Surveys from './surveys/components/Surveys'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      surveys: []
+    }
+  }
+
+  setSurveys = surveys => {
+    this.setState(surveys)
+  }
+
   render() {
     const theme = createMuiTheme({
       palette: {
@@ -14,15 +26,24 @@ class App extends React.Component {
         secondary: blue,
       },
     })
+
+    const {surveys} = this.state
+
     return (
       <React.Fragment>
         <MuiThemeProvider theme={theme}>
           <Router>
             <Header />
+            <Route exact path= '/surveys' render={props => (
+              <Surveys
+                surveys ={surveys}
+                setSurveys={this.setSurveys}
+              />
+            )} />
           </Router>
         </MuiThemeProvider>
       </React.Fragment>
-    );
+    )
   }
 }
 
