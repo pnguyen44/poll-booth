735ee198881eb266623acca9b749ce975d6bad78
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-05-17 22:51:50 -0400

    Update messages form option

--- src/options/messages.js
+++ src/options/messages.js
@@ -1,5 +1,6 @@
 export default {
   createOptionSuccess : 'Option added.',
   createOptionfailure: 'Error on create option',
+  updateOptionFailure: 'Error on submiting survey',
   deleteOptionFailure: 'Error on delete option'
 }
