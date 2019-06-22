ba2d6fd10348aef746721dbec0ad670df0068bcf
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-06-22 16:16:34 -0400

    Updates

---
+++ precache-manifest.7c5b32e84c9dd17f3e8ccc2a8153b3f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3b23a14ba2393a2b1bc3174f484d5d",
+    "url": "/poll-booth/index.html"
+  },
+  {
+    "revision": "19c15be886214abf6026",
+    "url": "/poll-booth/static/css/main.a030ee90.chunk.css"
+  },
+  {
+    "revision": "9d7c6dfdf61ab4630fe8",
+    "url": "/poll-booth/static/js/2.7423cfe3.chunk.js"
+  },
+  {
+    "revision": "19c15be886214abf6026",
+    "url": "/poll-booth/static/js/main.aa189325.chunk.js"
+  },
+  {
+    "revision": "8769656d2dbe9cdcf15f",
+    "url": "/poll-booth/static/js/runtime~main.9301b953.js"
+  },
+  {
+    "revision": "4083cb0d666ecfd7f04242937c9d99be",
+    "url": "/poll-booth/static/media/survey-icon.4083cb0d.png"
+  }
+]);
